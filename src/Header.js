5c9b29a3e4fb71de6ddee7f19703f0a30e03a2d7
Khristian Rojas <khristianrojas15@gmail.com>
2021-04-28 18:45:01 -0400

    Add Big, erase useless assets, import proptype

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.title}</h1>
+    );
+  }
+}
+
+Header.propTypes = {
+  name: PropTypes.string
+};
