5f05a186bb12ee699128a8641b076851a2b5b24b
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-02 18:53:58 -0400

    Refactor calculate to arrow func

---
+++ src/logic/calculate.js
@@ -0,0 +1,36 @@
+import operate from './operate';
+
+const calculate = (data, buttonName) => {
+  let { total, next, operation } = data;
+  let result;
+  const symbols = ['÷', '*', '-', '+'];
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+  if (buttonName === '+/-') {
+    result = {
+      total: total * -1,
+      next: next * -1,
+      operation: null,
+    };
+  }
+
+  if (buttonName === 'AC') {
+    result = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  if (buttonName === '%') {
+    result = {
+      total: total * (next / 100),
+      next: null,
+      operation: null,
+    };
+  }
+
+  return result;
+};
+
+export default calculate;
