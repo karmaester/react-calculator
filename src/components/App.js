ef1f4da21a88c26f05bae0b9b4df8c6b6bdd46c6
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-02 19:40:12 -0400

    Add handleClick, setCalculator and calculator

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
 /* eslint-disable no-unused-vars */
 import './App.css';
+import React, { useState } from 'react';
 import Header from './Header';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
 function App() {
+  const [calculator, setCalculator] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (operation) => {
+    setCalculator(calculate(calculator, operation));
+  };
+
+  const { total, next, operation } = calculator;
+
   return (
     <>
       <Header title="Math Magicians" />
       <div className="calculator">
-        <Display />
-        <ButtonPanel />
+        <Display
+          result={
+          `${total || ''}
+          ${operation || ''}
+          ${(total && next) || ''}`
+          }
+        />
+        <ButtonPanel clickHandler={handleClick} />
       </div>
     </>
   );
