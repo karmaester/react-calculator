6c25cb1b28e2d62ff190e35b0c182648d33f8e56
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-02 18:23:00 -0400

    Add react fragment to App

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,13 @@ import ButtonPanel from './ButtonPanel';
 function App() {
   const x = new Big(123.4567);
   return (
-    <div className="App">
+    <>
       <Header title="Math Magicians" />
       <div className="calculator">
         <Display />
         <ButtonPanel />
       </div>
-    </div>
+    </>
   );
 }
 
