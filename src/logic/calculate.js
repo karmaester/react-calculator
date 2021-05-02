8c6f6af1bcccf6b3fcd30332b12dfd1d355dd9c5
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-02 19:01:29 -0400

    Add operate method to calculate total

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -6,6 +6,14 @@ const calculate = (data, buttonName) => {
   const symbols = ['÷', '*', '-', '+'];
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
+  if (buttonName === '=') {
+    result = {
+      total: operate(total, next, operation),
+      next: null,
+      operation: null,
+    };
+  }
+
   if (buttonName === '+/-') {
     result = {
       total: total * -1,
