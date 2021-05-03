3db4c0b96a85ac6b0be0d49a6fcc9c6c8d5aa14c
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-03 16:04:57 -0400

    Add onClick to ButtonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -8,27 +8,31 @@ const nums3 = ['4', '5', '6', '-'];
 const nums4 = ['1', '2', '3', '+'];
 const nums5 = ['0', '.', '='];
 
-const ButtonPanel = ({ clickHandler }) => (
-  <div className="display">
-    <>
-      {nums1.map((num) => (
-        <Button name={num} key={num} handleClick={clickHandler} />
-      ))}
-      {nums2.map((num) => (
-        <Button name={num} key={num} handleClick={clickHandler} />
-      ))}
-      {nums3.map((num) => (
-        <Button name={num} key={num} handleClick={clickHandler} />
-      ))}
-      {nums4.map((num) => (
-        <Button name={num} key={num} handleClick={clickHandler} />
-      ))}
-      {nums5.map((num) => (
-        <Button name={num} key={num} handleClick={clickHandler} />
-      ))}
-    </>
-  </div>
-);
+const ButtonPanel = ({ clickHandler }) => {
+  const onClick = (buttonName) => clickHandler(buttonName);
+
+  return (
+    <div className="display">
+      <>
+        {nums1.map((num) => (
+          <Button name={num} key={num} handleClick={onClick} />
+        ))}
+        {nums2.map((num) => (
+          <Button name={num} key={num} handleClick={onClick} />
+        ))}
+        {nums3.map((num) => (
+          <Button name={num} key={num} handleClick={onClick} />
+        ))}
+        {nums4.map((num) => (
+          <Button name={num} key={num} handleClick={onClick} />
+        ))}
+        {nums5.map((num) => (
+          <Button name={num} key={num} handleClick={onClick} />
+        ))}
+      </>
+    </div>
+  );
+};
 
 ButtonPanel.propTypes = {
   clickHandler: PropTypes.func.isRequired,
