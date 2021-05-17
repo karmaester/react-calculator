ba7aec6b71951333a416b200df110216b80ee674
Khristian Rojas <khristianrojas15@gmail.com>
2021-05-17 13:36:33 -0400

    Add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,20 @@
 /* eslint-disable no-unused-vars */
 import './App.scss';
-import React, { useState } from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
+import { Route, Switch } from 'react-router-dom';
+import Calculator from './Calculator';
+import Home from './Home';
+import Quote from './Quote';
+import Navbar from './Navbar';
 
-const App = () => {
-  const [value, setValue] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-
-  const clickHandler = (buttonName) => {
-    const newResult = calculate(value, buttonName);
-    setValue({
-      ...newResult,
-    });
-  };
-
-  return (
-    <>
-      <div className="calculator">
-        <Display
-          result={
-            `${value.total || ''}
-            ${value.operation || ''}
-            ${(value.total && value.next) || ''}`
-            }
-        />
-        <ButtonPanel clickHandler={clickHandler} />
-      </div>
-    </>
-  );
-};
+const App = () => (
+  <main>
+    <Navbar />
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/Calculator" component={Calculator} exact />
+      <Route path="/Quote" component={Quote} exact />
+    </Switch>
+  </main>
+);
 export default App;
 /* eslint-enable no-unused-vars */
---
+++ src/components/Home.js
@@ -0,0 +1,5 @@
+const Home = () => (
+  <div className="welcome">Welcome to Math Magicians</div>
+);
+
+export default Home;
